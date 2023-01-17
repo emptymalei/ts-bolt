52e8b4ca1fb32cf6de9f8b7d4dede2cb6a758369
LM <hi@leima.is>
2023-01-17 22:06:26 +0100

    update tests

--- tests/integration/datamodules/test_gluonts.py
+++ tests/integration/datamodules/test_gluonts.py
@@ -35,7 +35,7 @@ def test_gluonts_dataset(gluonts_dataset, datamodules_artefacts_dir):
 
     if is_regenerate_artefact:
         with open(expected_dataset_path, "wb+") as fp:
-            pickle.dump(g_ds_train, fp)
+            pickle.dump(list(g_ds_train), fp)
 
     with open(expected_dataset_path, "rb") as fp:
         g_ds_train_expected = pickle.load(fp)
@@ -47,7 +47,10 @@ def test_gluonts_datamodule(gluonts_dataset, gluonts_dataloader_config, datamodu
 
     is_regenerate_artefact = False
 
-    expected_dataloader_path = datamodules_artefacts_dir / "gluonts_dataset_to_torch_dataloader_expected.pkl"
+    expected_train_dataloader_path = (
+        datamodules_artefacts_dir / "gluonts_dataset_to_torch_train_dataloader_expected.pkl"
+    )
+    expected_test_dataloader_path = datamodules_artefacts_dir / "gluonts_dataset_to_torch_test_dataloader_expected.pkl"
 
     dm = GluonTSDataModule(
         gluonts_dataset=gluonts_dataset,
@@ -56,17 +59,23 @@ def test_gluonts_datamodule(gluonts_dataset, gluonts_dataloader_config, datamodu
     )
 
     if is_regenerate_artefact:
-        with open(expected_dataloader_path, "wb+") as fp:
-            pickle.dump(dm, fp)
+        with open(expected_train_dataloader_path, "wb+") as fp:
+            pickle.dump(list(dm.train_dataloader()), fp)
 
-    with open(expected_dataloader_path, "rb") as fp:
-        dm_expected = pickle.load(fp)
+        with open(expected_test_dataloader_path, "wb+") as fp:
+            pickle.dump(list(dm.test_dataloader()), fp)
 
-    assert len(dm.train_dataloader()) == len(dm_expected.train_dataloader())
+    with open(expected_train_dataloader_path, "rb") as fp:
+        dm_train_dataloader_expected = pickle.load(fp)
+
+    with open(expected_test_dataloader_path, "rb") as fp:
+        dm_test_dataloader_expected = pickle.load(fp)
+
+    assert len(dm.train_dataloader()) == len(dm_train_dataloader_expected)
+    assert len(dm.test_dataloader()) == len(dm_test_dataloader_expected)
 
     dm_train_dataloader_values = list(dm.train_dataloader())
-    dm_expected_train_dataloader_values = list(dm_expected.train_dataloader())
 
-    for i, j in zip(dm_train_dataloader_values, dm_expected_train_dataloader_values):
+    for i, j in zip(dm_train_dataloader_values, dm_train_dataloader_expected):
         for k in ("target", "feat_static_cat", "feat_static_real"):
             torch.testing.assert_close(i[k], j[k])
