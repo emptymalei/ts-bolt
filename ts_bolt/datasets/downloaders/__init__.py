15f38b20b580e5e6ebbcca004d1e661aeb370463
LM <hi@leima.is>
2023-10-17 23:36:09 +0200

    adjust dependencies

---
+++ ts_bolt/datasets/downloaders/__init__.py
@@ -0,0 +1,4 @@
+from ts_bolt.datasets.downloaders.base import BaseDownloader
+from ts_bolt.datasets.downloaders.ecb_exchange_rate import ECBExchangeRateDownloader
+
+__all__ = ["BaseDownloader", "ECBExchangeRateDownloader"]
